b19e58e87349a6a4d2a29699fb7e901229f7a0d3
ConnorBStill <connor@lamarsoftware.io>
2025-01-20 15:33:14 -0800

    Fix drag and drop highlighting when hovering child elements

--- src/components/ui/file-dropzone.tsx
+++ src/components/ui/file-dropzone.tsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 import { buttonVariants } from "@/components/ui/button";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 export default function FileDropzone() {
-  const [borderColor, setBorderColor] = useState('border-stone-500');
+  const [dragCounter, setDragCounter] = useState(0)
 
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
@@ -18,14 +18,16 @@ export default function FileDropzone() {
   const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     e.stopPropagation();
-    setBorderColor('border-primary')
+
+    setDragCounter(prevVal => prevVal + 1);
     console.log('handleDragEnter', e)
   }
 
   const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     e.stopPropagation();
-    setBorderColor('border-stone-500')
+
+    setDragCounter(prevVal => prevVal - 1);
   }
 
   const handleDragDrop = (e: React.DragEvent<HTMLDivElement>) => {
@@ -40,9 +42,23 @@ export default function FileDropzone() {
       onDragLeave={handleDragLeave}
       onDragOver={handleDragOver}
       onDrop={handleDragDrop}
-      className={`flex flex-col justify-between items-center bg-white/10 p-3 w-72 h-52 border-2 border-dashed ${borderColor} rounded-lg`}>
-        <span>Drag and Drop</span>
-        <span>or</span>
+      className={
+        `flex flex-col justify-between items-center bg-white/10 p-3 w-72 h-52 border-2 border-dashed ${dragCounter ? 'border-primary' : 'border-stone-500'} rounded-lg`
+      }>
+        <span
+          onDragEnter={handleDragEnter}
+          onDragOver={handleDragOver}
+        >
+          Drag and Drop
+        </span>
+
+        <span 
+          onDragEnter={handleDragEnter}
+          onDragOver={handleDragOver}
+        >
+          or
+        </span>
+
         <Button>Upload Files</Button>
     </div>
   );
