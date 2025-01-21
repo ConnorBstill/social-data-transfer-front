cb39bc8d08db6b49893a7a7708c53e125b1121d6
ConnorBStill <connor@lamarsoftware.io>
2025-01-20 19:54:09 -0800

    Parse folder when dropped

--- src/components/ui/file-dropzone.tsx
+++ src/components/ui/file-dropzone.tsx
@@ -2,13 +2,25 @@
 
 import Link from "next/link";
 
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
+import React, { useRef, useState, type ChangeEvent } from "react";
 
-import { buttonVariants } from "@/components/ui/button";
-import React, { useRef, useState } from "react";
+import { getAllFileEntries } from '../../lib/utils';
+
+declare module "react" {
+  interface InputHTMLAttributes<T> extends HTMLAttributes<T> {
+      webkitdirectory?: string;
+  }
+}
+
+interface Folder {
+  name: string;
+  files: File[]
+}
 
 export default function FileDropzone() {
-  const [dragCounter, setDragCounter] = useState(0)
+  const [dragCounter, setDragCounter] = useState(0);
+  const [uploadedFolders, setUploadedFolders] = useState<Folder[]>([]);
 
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
@@ -20,7 +32,6 @@ export default function FileDropzone() {
     e.stopPropagation();
 
     setDragCounter(prevVal => prevVal + 1);
-    console.log('handleDragEnter', e)
   }
 
   const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
@@ -30,10 +41,67 @@ export default function FileDropzone() {
     setDragCounter(prevVal => prevVal - 1);
   }
 
-  const handleDragDrop = (e: React.DragEvent<HTMLDivElement>) => {
+  const handleDrop = async (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     e.stopPropagation();
-    console.log('drop', e.dataTransfer.files)
+
+    console.log('e.dataTransfer', e.dataTransfer.items)
+    const items = e.dataTransfer?.items
+
+    const files = await getAllFileEntries(items);
+
+    console.log('files', files)
+  }
+
+  const handleFileButtonUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    e.stopPropagation()
+    console.log('file CHANGE', e)
+  }
+
+  const renderDropZoneContent = () => {
+    if (!uploadedFolders.length) {
+      return (
+        <>
+          <span
+            onDragEnter={handleDragEnter}
+            onDragOver={handleDragOver}
+          >
+            Drag and Drop
+          </span>
+  
+          <span 
+            onDragEnter={handleDragEnter}
+            onDragOver={handleDragOver}
+          >
+            or
+          </span>
+  
+          <label 
+            htmlFor="file-upload" 
+            className={
+              `cursor-pointer ${buttonVariants({ variant: "default" })}`
+            }>
+            <input
+              onChange={handleFileButtonUpload}
+              type="file" 
+              id="file-upload" 
+              className="hidden-input"
+              webkitdirectory="true"
+              multiple />
+
+            Upload Files
+          </label>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <span>Uploaded file(s):</span>
+          {uploadedFolders.map(({ name }) => <span>{name}</span>)}
+        </>
+      )
+    }
   }
 
   return (
@@ -41,25 +109,11 @@ export default function FileDropzone() {
       onDragEnter={handleDragEnter}
       onDragLeave={handleDragLeave}
       onDragOver={handleDragOver}
-      onDrop={handleDragDrop}
+      onDrop={handleDrop}
       className={
         `flex flex-col justify-between items-center bg-white/10 p-3 w-72 h-52 border-2 border-dashed ${dragCounter ? 'border-primary' : 'border-stone-500'} rounded-lg`
       }>
-        <span
-          onDragEnter={handleDragEnter}
-          onDragOver={handleDragOver}
-        >
-          Drag and Drop
-        </span>
-
-        <span 
-          onDragEnter={handleDragEnter}
-          onDragOver={handleDragOver}
-        >
-          or
-        </span>
-
-        <Button>Upload Files</Button>
+        {renderDropZoneContent()}
     </div>
   );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,57 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getAllFileEntries = async (items: DataTransferItemList): Promise<FileSystemEntry[]> => {
+  const files: FileSystemEntry[] = [];
+
+  const queue: (FileSystemEntry | FileSystemDirectoryEntry)[] = [];
+
+  for (let i = 0; i< items.length; i++) {
+    const item: DataTransferItem = items[i];
+
+    if (item) {
+      queue.push(item.webkitGetAsEntry()!);
+    }
+  }
+
+  while (queue.length) {
+    const entry = queue.shift();
+
+    if (entry?.isFile) {
+      const fileEntry = entry as FileSystemEntry
+      files.push(fileEntry);
+    } else if (entry?.isDirectory) {
+      const dirEntry = entry as FileSystemDirectoryEntry
+      const reader = dirEntry.createReader();
+
+      queue.push(...await readAllDirectoryEntries(reader))
+    }
+  }
+
+  return files;
+}
+
+const readAllDirectoryEntries = async (reader: FileSystemDirectoryReader) => {
+  const entries: FileSystemEntry[] = [];
+
+  let readEntries: FileSystemEntry[] = await readEntriesPromise(reader);
+
+  while (readEntries.length) {
+    entries.push(...readEntries);
+
+    readEntries = await readEntriesPromise(reader);
+  }
+
+  return entries;
+}
+
+const readEntriesPromise = async (reader: FileSystemDirectoryReader): Promise<any> => {
+  try {
+    return await new Promise((resolve, reject) => {
+      reader.readEntries(resolve, reject)
+    });
+  } catch (err) {
+    console.log('readEntriesPromise error', err)
+  }
+}
